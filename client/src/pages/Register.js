d8db83feb63463c24db86422754914b2b547352a
Chris Abbondandelo <abbondcj@gmail.com>
2023-08-22 19:52:36 -0400

    ensure it runs locally

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -16,7 +16,7 @@ const Register = () => {
     const username = userUsername
     const email = userEmail
     const password = userPassword
-    fetch('https://soundwriteai.com:9000/register', {
+    fetch('http://localhost:9000/register', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
